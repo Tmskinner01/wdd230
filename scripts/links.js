ea577ad1ee6f38136e48cc8069a1d7c819efa750
Todd Skinner <tmskinner01@gmail.com>
2023-10-11 20:50:58 -0400

    finished group assignment

--- scripts/links.js
+++ scripts/links.js
@@ -1,16 +1,38 @@
 const baseURL = "https://tmskinner01.github.io/wdd230/";
 const linksURL = "https://tmskinner01.github.io/wdd230/data/links.json";
+const linkList = document.querySelector("#link-list");
 
 async function getData() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
     displayLinks(data);
 
 };
 function displayLinks(data) {
-    weeks.links.forEach(element => {
+    data.weeks.forEach(week => {
+        const weekText = document.createElement("li");
         
+
+        weekText.innerText = `${week.week}: `;
+        const frontLinks = week.links.slice(1);
+
+        week.links.forEach(link => {
+            const a = document.createElement("a");
+            a.setAttribute("href", link.url);
+            a.setAttribute("target", "_blank");
+            a.textContent = link.title;
+           
+            if (frontLinks.includes(link)) {
+                let spacer = document.createElement("span")
+                spacer.textContent = ` | `;
+                weekText.appendChild(spacer);
+            }
+
+            weekText.appendChild(a);
+            
+        });
+
+        linkList.appendChild(weekText);
     });
 };
 
